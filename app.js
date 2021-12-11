3fccc365e6ebc562b82bdb5698f74ceb9f2059f8
DrHyde111 <pawel.j.skalski@gmail.com>
2021-12-11 16:21:21 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
