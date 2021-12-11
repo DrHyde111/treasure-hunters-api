ab3085e4ca7cba325a4cb43ec51d8d326b9c802d
DrHyde111 <pawel.j.skalski@gmail.com>
2021-12-11 18:44:05 +0100

    Implemented create user controller logi

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,58 +2,66 @@ const db = require("../models")
 const User = db.user
 
 // Create and Save a new User
-exports.create = (req, res) => {
-    try{
-        const User = new User({
-            email: req.body.email
-
+exports.create = async (req, res) => {
+    try {
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password,
+            name: req.body.name,
+            role: req.body.role
         })
-    }catch (error){
+        await User.create(user, function (err, user) {
+            if (err) {
+                return res.status(500).send({message: "Error during creation of user", user: null})
+            }
+            return res.status(200).send({message: "User was created", user: user})
+        })
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
 
 // Retrieve all Users from the database.
 exports.findAll = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
 
 // Find a single User with an id
 exports.findOne = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
 
 // Update a User by the id in the request
 exports.update = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
 
 // Delete a User with the specified id in the request
 exports.delete = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
 
 // Delete all Users from the database.
 exports.deleteAll = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 };
