5a745e736724ac9a3a2c2d2025b27035e16bd8bc
DrHyde111 <pawel.j.skalski@gmail.com>
2021-12-11 19:20:23 +0100

    Initialised user routes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({extended: true}));
 
 db.initConnection()
 
+require("./app/routes/tutorial.routes")(app);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
