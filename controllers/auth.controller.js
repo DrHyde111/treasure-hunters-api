139bcfabedf84ab719d2f254478c9ded3ff2d660
DrHyde111 <pawel.j.skalski@gmail.com>
2022-01-22 16:47:03 +0100

    Implemented login endpoint controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,21 +5,23 @@ const bcrypt = require("../services/encryption.service")
 exports.login = async (req, res) => {
     try {
         let user = await User.findOne({email: req.body.email})
-        if(!user){
+        if (!user) {
             return res.status(400).send({message: "Invalid credentials"})
         }
         let result = await bcrypt.comparePassword(req.body.password, user.password)
-
-
+        if (!result) {
+            return res.status(400).send({message: "Invalid credentials"})
+        }
+        return res.status(400).send({message: "Login successfull", user: user})
     } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 }
 
 exports.register = (req, res) => {
-    try{
+    try {
 
-    }catch (error){
+    } catch (error) {
         return res.status(500).send({message: "Something went wrong."})
     }
 }
